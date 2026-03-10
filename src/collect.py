5149ab1226284e5e6dbcb1715d00d37b058777db
Felipe Santos <ofelipedsnts@gmail.com>
2026-03-10 11:08:14 -0300

    fix(collect): implement early stopping in process loop

--- src/collect.py
+++ src/collect.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 import fastf1
 import pandas as pd
 import time
@@ -6,18 +6,18 @@ import argparse
 
 pd.set_option('display.max_columns', None)
 
-#%%
+# %%
+
 
 class Collect:
     def __init__(self, years=[2021, 2022, 2023, 2024, 2025], modes=["R", "S"]):
         self.years = years
         self.modes = modes
 
-    
     def get_data(self, year, gp, mode) -> pd.DataFrame():
         try:
             session = fastf1.get_session(year, gp, mode)
-        
+
         except ValueError as err:
             return pd.DataFrame()
 
@@ -36,30 +36,32 @@ class Collect:
 
         return df
 
-
     def save_data(self, df, year, gp, mode):
-        filename = f"data/{year}-{gp:02}-{mode}.parquet"
+        filename = f"data/{year}_{gp:02}_{mode}.parquet"
         df.to_parquet(filename, index=False)
 
+    def process(self, year, gp, mode):
+        df = self.get_data(year, gp, mode)
 
-    def process(self, year):
-        for i in range(1,30):
+        if df.empty:
+            return False
 
-            for m in self.modes: 
-                df = self.get_data(year, i, m)
-                time.sleep(1)
+        self.save_data(df, year, gp, mode)
+        time.sleep(1)
+        return True
 
-                if len(df) == 0:
-                    continue
+    def process_year_modes(self, year):
+        for i in range(1, 30):
+            for mode in self.modes:
+                if not self.process(year, i, mode) and mode == "R":
+                    return
 
-                self.save_data(df, year, i, m)
-
-    
-    def extract_data(self):
+    def process_years(self):
         for year in self.years:
             print(f"coletando dados do ano {year}")
-            self.process(year)
-            time.sleep(5)
+            self.process_year_modes(year)
+            time.sleep(10)
+
 
 # %%
 if __name__ == "__main__":
@@ -77,9 +79,7 @@ if __name__ == "__main__":
     elif args.start and args.stop:
         years = []
 
-        for i in range(args.start, args.stop):
-            years.append(i)
+        years = [i for i in range(args.start, args.stop + 1)]
+        collect = Collect(years, args.modes)
 
-        collect = Collect(years, args.modes)    
-
-    collect.extract_data()
+    collect.process_years()
